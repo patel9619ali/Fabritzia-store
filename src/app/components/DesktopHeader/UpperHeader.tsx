80687c4e1c34ffbf7c7b4f3225d3a45efefcec8f
patel9619ali <patel9619ali@gmail.com>
2024-09-21 14:10:22 +0530

    testing

--- src/app/components/DesktopHeader/UpperHeader.tsx
+++ src/app/components/DesktopHeader/UpperHeader.tsx
@@ -3,12 +3,10 @@ import SearchBox from '../DesktopHeader/SearchBox/SearchBox'
 import AddToCart from '../DesktopHeader/AddToCart/AddToCart'
 export default function UpperHeader(){
     return(
-        <>
         <div className='row'>
             <Logo className="col-lg-3"/>
             <SearchBox className="col-lg-6 text-center"/>
             <AddToCart className="col-lg-3"/>
         </div>
-        </>
     )
 }
