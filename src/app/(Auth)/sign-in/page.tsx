3c852fa726ede2cadf277273d7d108f40369086a
patel9619ali <patel9619ali@gmail.com>
2024-10-12 03:39:28 +0530

    reacthook-design-started

--- src/app/(Auth)/sign-in/page.tsx
+++ src/app/(Auth)/sign-in/page.tsx
@@ -1,21 +1,56 @@
 'use client'
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import styled from 'styled-components';
+import Logo from '@/app/components/DesktopHeader/Logo/Logo';
+const FormWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background: #80808014;
+    height: 100vh;
+    .common_input_holder{
+      display: block;
+      margin-bottom: 12px;
+    }
+    .input_error{
+      margin-bottom: 0px; 
+      }
+`;
+const FormMainWrapper = styled.form`
+  padding: 36px 36px 0;
+  border-radius: 15px;
+  box-shadow: 0px 15px 20px rgba(0,0,0,0.1);
+  background: #fff;
+`;
+const ErrorMessage = styled.div`
+  font-size: 12px;
+  color: #ff0000;
+  display: inline;
+  ul{
+    list-style: auto;
+    padding-left: 12px;
+  }
+`;
 export default function LoginPassword() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = (data:any) => console.log(data,"data");
-    console.log(errors,"error");
     
     return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
-        <input type="password" placeholder="Password" {...register("Password", {required: true, max: 15, min: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
-        })}
-      />
-  
+      <FormWrapper>
+         <Logo className=''/>
+        <FormMainWrapper onSubmit={handleSubmit(onSubmit)} className='mt-3'>
+          <input className={`common_input_holder ${errors["Email"] ? 'input_error' : ''}`} type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+          {errors["Email"] && <ErrorMessage>Please Provide the Valid Email</ErrorMessage>}
+          <input className={`common_input_holder ${errors["Mobile number"] ? 'input_error' : ''}`} type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+          {errors["Mobile number"] && <ErrorMessage>Please write Valid Phone Number</ErrorMessage>}
+          <input className={`common_input_holder ${errors["Password"] ? 'input_error' : ''}`} type="password" placeholder="Password" {...register("Password", {required: true, max: 15, min: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+          })}
+        />
+        {errors["Password"] && <ErrorMessage>Password should contain <ul><li>Number</li><li>Capital letter</li><li>Small letter</li><li>Special characters</li><li>Should be between 8 to 15 Characters long</li></ul></ErrorMessage>}
         <input type="submit" />
-      </form>
+        </FormMainWrapper>
+      </FormWrapper>
     );
   }
--- src/app/(Auth)/sign-up/page.tsx
+++ src/app/(Auth)/sign-up/page.tsx
@@ -1,27 +1,74 @@
 'use client'
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import styled from 'styled-components';
+import Logo from '@/app/components/DesktopHeader/Logo/Logo';
 
-export default function SignUp() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data:any) => console.log(data,"data");
-    console.log(errors,"error");
-    const password = watch("Password");
-    
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="First name" {...register("First name", {required: true, minLength: 6, maxLength: 12})} />
-        <input type="text" placeholder="Last name" {...register("Last name", {required: true, minLength: 6, maxLength: 12})} />
-        <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
-        <input type="password" placeholder="Password" {...register("Password", {required: true, max: 15, min: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
-        })}
-      />
-        <input type="password" placeholder="Confirm Password" {...register("Confirm Password", {required: true, max: 15, min: 5, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
-        })}
-      />
-  
-        <input type="submit" />
-      </form>
-    );
+type FormData = {
+  "First name": string;
+  "Last name": string;
+  "Email": string;
+  "Mobile number": string;
+  "Password": string;
+  "Confirm Password": string;
+};
+const FormWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background: #80808014;
+    .common_input_holder{
+      display: block;
+      margin-bottom: 12px;
+    }
+    .input_error{
+      margin-bottom: 0px; 
+      }
+`;
+const FormMainWrapper = styled.form`
+  padding: 36px 36px 0;
+  border-radius: 15px;
+  box-shadow: 0px 15px 20px rgba(0,0,0,0.1);
+  background: #fff;
+`;
+const ErrorMessage = styled.div`
+  font-size: 12px;
+  color: #ff0000;
+  display: inline;
+  ul{
+    list-style: auto;
+    padding-left: 12px;
   }
+`;
+export default function SignUp() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+    const onSubmit: SubmitHandler<FormData> = (data) => console.log(data, "data");
+    const password = watch("Password");
+
+    return (
+      <FormWrapper>
+        <Logo className='mt-3'/>
+        <FormMainWrapper onSubmit={handleSubmit(onSubmit)} className='mt-3'>
+          <input className={`common_input_holder ${errors["First name"] ? 'input_error' : ''}`} type="text" placeholder="First name" {...register("First name", { required: true, minLength: 6, maxLength: 12 })} />
+          {errors["First name"] && <ErrorMessage>First name should be between 6 to 12 length</ErrorMessage>}
+          
+          <input className={`common_input_holder ${errors["Last name"] ? 'input_error' : ''}`} type="text" placeholder="Last name" {...register("Last name", { required: true, minLength: 6, maxLength: 12 })} />
+          {errors["Last name"] && <ErrorMessage>Last Name should be between 6 to 12 length</ErrorMessage>}
+          
+          <input className={`common_input_holder ${errors["Email"] ? 'input_error' : ''}`} type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
+          {errors["Email"] && <ErrorMessage>Please write Valid Email ID</ErrorMessage>}
+          
+          <input className={`common_input_holder ${errors["Mobile number"] ? 'input_error' : ''}`} type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
+          {errors["Mobile number"] && <ErrorMessage>Please write Valid Phone Number</ErrorMessage>}
+          
+          <input className={`common_input_holder ${errors["Password"] ? 'input_error' : ''}`} type="password" placeholder="Password" {...register("Password", { required: true, maxLength: 15, minLength: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/ })} />
+          {errors["Password"] && <ErrorMessage>Password should contain <ul><li>Number</li><li>Capital letter</li><li>Small letter</li><li>Special characters</li><li>Should be between 8 to 15 Characters long</li></ul></ErrorMessage>}
+          
+          <input className={`common_input_holder ${errors["Confirm Password"] ? 'input_error' : ''}`} type="password" placeholder="Confirm Password" {...register("Confirm Password", { required: true, maxLength: 15, minLength: 5, validate: value => value === password || "Passwords do not match" })} />
+          {errors["Confirm Password"] && <ErrorMessage>{errors["Confirm Password"].message}</ErrorMessage>}
+          <input className="common_input_holder" type="submit" />
+        </FormMainWrapper>
+      </FormWrapper>
+    );
+}
